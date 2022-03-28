76340d73b64b2af8291e9cf4fecd181a1bb111b9
Jesus898 <jesus81398@gmail.com>
2022-03-28 15:12:35 -0500

    completed dashboard component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Dashboard from "./Dashboard";
+// import './App.css';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    padding: theme.spacing(1),
+  },
+  root: {
+    marginTop: theme.spacing(10),
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  formContainer: {
+    // marginTop: theme.spacing(10),
+    width: 300,
+    margin: "auto",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [loggedIn, setLogin] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* nav bar component */}
+      <AppBar className={classes.appBar}>My Music App</AppBar>
+      <div className={classes.root}>
+        {loggedIn ? (
+          <Dashboard />
+        ) : (
+          
+          // login component
+          <div className={classes.formContainer}>
+            <form className={classes.form} noValidate autoComplete="off">
+              <TextField id="standard-basic" label="Username" />
+              <TextField id="standard-basic" label="Password" />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  setLogin(true);
+                }}
+              >
+                Login
+              </Button>
+            </form>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/Dashboard.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Switch from '@material-ui/core/Switch';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import './Dashboard.css';
+
+class Dashboard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOnline: false,
+            volume: 80,
+            quality: 'medium'
+        }
+    }
+
+    onClickHandler = (event) => {
+        this.setState({ isOnline : event.target.checked })
+    }
+
+    handleVolumeChange = (event, volume) => {
+        this.setState({ volume })
+        
+    }
+
+    handleDragStop = () => {
+        this.props.update(this.state.volume)
+    }
+
+    handleQualityChange = (event) => {
+        this.setState({ quality : event.target.value })
+    }
+
+    render() {
+        
+        return (
+            <div className="Dashboard">
+                <Card className="cardContainer">
+                    <CardContent>
+                        <Typography variant="h5">Online Mode</Typography>
+                        <Typography variant="body2">App connected to the internet?</Typography>
+                    </CardContent>
+                    <Switch onClick={this.onClickHandler} value={this.state.isOnline} />
+                </Card>
+
+                <Card className="cardContainer">
+                    <CardContent>
+                        <Typography variant="h5">Master Volume</Typography>
+                        <Typography variant="body2">Overrides all sound settings.</Typography>
+                    </CardContent>
+                    <Slider onChange={this.handleVolumeChange} onDragStop={this.handleDragStop} defaultValue={this.state.volume} valueLabelDisplay="auto" step={10} min={0} max={100} />
+                </Card>
+
+                <Card className="cardContainer">
+                    <CardContent>
+                        <Typography variant="h5">Sound Quality</Typography>
+                        <Typography variant="body2">Manually control music quality.</Typography>
+                    </CardContent>
+                    <FormControl variant="filled">
+                        <InputLabel className="inputLabel">Quality</InputLabel>
+                        <Select className="qualityButton" onChange={this.handleQualityChange} value={this.state.quality}>
+                            <MenuItem value={'Low'}>Low</MenuItem>
+                            <MenuItem value={'Medium'}>Medium</MenuItem>
+                            <MenuItem value={'High'}>High</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Card>
+                
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
